aa57914f8b1e9f24d877ffc0fc35a3f91e066e39
jialin <mr.liujalo@gmail.com>
2025-01-09 13:15:29 +0800

    1.13 前端项目显示首页环境搭建 配置全局settings

---
+++ src/settings.js
@@ -0,0 +1,5 @@
+// 在src目录下创建settings.js站点开发配置文件 用于配置开发环墨的一些配置信息
+// 在main.js中引入使用
+export default {
+  Host: "http://127.0.0.1:8000",
+};
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <Header></Header>
+    <Banner></Banner>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script setup>
+import Banner from "./common/Banner.vue";
+import Footer from "./common/Footer.vue";
+import Header from "./common/Header.vue";
+</script>
+
+<style scoped></style>
